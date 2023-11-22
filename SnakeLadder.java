1bed449d3391a40f29b93941e10ed2332d21f936
Kriti Jaiswal <kritijaiswal2901@yahoo.com>
2023-11-23 00:49:28 +0530

    Dice Number

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -1,6 +1,16 @@
+import java.util.Random;
+
+
 public class SnakeLadder {
-public static void main(String[] args) {
-    System.out.println("Welcome to Snake and Ladder game");
+    int getDiceNumber() {
+        Random random = new Random();
+        int num = random.nextInt(6) + 1;
+        return num;
+    }
     
-}
+    public static void main(String[] args) {
+        System.out.println("Welcome to Snake and Ladder game");
+        SnakeLadder player1 = new SnakeLadder();
+        System.out.println("Player 1 got: " + player1.getDiceNumber());
+    }
 }
